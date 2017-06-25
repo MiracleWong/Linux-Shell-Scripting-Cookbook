6c10b433612d35f5ee9382c84b78e1f4ab84ce98
MiracleWong <cfwr1991@126.com>
2017-06-26 00:33:59 +0800

    auto ssh-copy-id & md5check

---
+++ chapter18/expect_ssh_copy_id.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PASS=$1
+IP=$2
+auto_ssh_copy_id(){
+  expect -c "set timeout -1;
+  spawn /usr/bin/ssh-copy-id -i /root/.ssh/id_rsa.pub root@$2;
+  expect {
+    *(yes/no)* {send -- yes\r;exp_continue;}
+    *Password:* {send -- $1\r;exp_continue;}
+    eof  {exit 0;}
+  }";
+}
+auto_ssh_copy_id $PASS $IP
