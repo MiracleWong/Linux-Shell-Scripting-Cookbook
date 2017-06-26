db3d6a032593a508dbcb76bfe52db10af1da56e5
MiracleWong <cfwr1991@126.com>
2017-06-26 23:43:03 +0800

    add the mysql bash script

--- chapter18/expect_ssh_copy_id.sh
+++ chapter18/expect_ssh_copy_id.sh
@@ -3,7 +3,7 @@ PASS=$1
 IP=$2
 auto_ssh_copy_id(){
   expect -c "set timeout -1;
-  spawn /usr/bin/ssh-copy-id -i /root/.ssh/id_rsa.pub root@$2;
+  spawn /usr/bin/ssh-copy-id -i /root/.ssh/id_rsa.pub ubuntu@$2;
   expect {
     *(yes/no)* {send -- yes\r;exp_continue;}
     *Password:* {send -- $1\r;exp_continue;}
